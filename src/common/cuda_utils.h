6acefdeb76633f2d27ba335b1fe0f37edc6f7b98
marcus <1175677541@qq.com>
2026-05-24 19:43:52 +0800

    adjust

---
+++ src/common/cuda_utils.h
@@ -0,0 +1,6 @@
+#define WARP_SIZE          32
+#define INT4(value)        (reinterpret_cast<int4 *>(&(value))[0])
+#define FLOAT4(value)      (reinterpret_cast<float4 *>(&(value))[0])
+#define HALF2(value)       (reinterpret_cast<half2 *>(&(value))[0])
+#define BFLOAT2(value)     (reinterpret_cast<__nv_bfloat162 *>(&(value))[0])
+#define LDST128BITS(value) (reinterpret_cast<float4 *>(&(value))[0])
--- src/ops/elementwise/elementwise_add.cuh
+++ src/ops/elementwise/elementwise_add.cuh
@@ -1,13 +1,7 @@
 #pragma once
 
 #include <cuda_fp16.h>
-
-#define WARP_SIZE          32
-#define INT4(value)        (reinterpret_cast<int4 *>(&(value))[0])
-#define FLOAT4(value)      (reinterpret_cast<float4 *>(&(value))[0])
-#define HALF2(value)       (reinterpret_cast<half2 *>(&(value))[0])
-#define BFLOAT2(value)     (reinterpret_cast<__nv_bfloat162 *>(&(value))[0])
-#define LDST128BITS(value) (reinterpret_cast<float4 *>(&(value))[0])
+#include "../../common/cuda_utils.h"
 
 __global__ void elementwise_add_f32_kernel(float *a, float *b, float *c, int N);
 __global__ void elementwise_add_f32x4_kernel(float *a, float *b, float *c,
