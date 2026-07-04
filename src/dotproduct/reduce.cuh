4e890a4594f58db0070665a30b34b3a6794b6291
marcus <1175677541@qq.com>
2026-07-04 23:57:57 +0800

    dot product init

---
+++ src/dotproduct/reduce.cuh
@@ -0,0 +1,21 @@
+#include "../common/cuda/cuda_utils.h"
+#include <cuda_fp16.h>
+
+template <const int kWarpSize = WARP_SIZE>
+__device__ __forceinline__ float warp_reduce_sum_f32(float val) {
+#pragma unroll
+    for (int mask = kWarpSize >> 1; mask >= 1; mask >>= 1) {
+        val += __shfl_xor_sync(0xffffffff, val, mask);
+    }
+    return val;
+}
+
+template <const int kWarpSize = WARP_SIZE>
+__device__ __forceinline__ float warp_reduce_sum_f16_f32(half val) {
+    float val_f32 = __half2float(val);
+#pragma unroll
+    for (int mask = kWarpSize >> 1; mask >= 1; mask >>= 1) {
+        val_f32 += __shfl_xor_sync(0xffffffff, val_f32, mask);
+    }
+    return val_f32;
+}
