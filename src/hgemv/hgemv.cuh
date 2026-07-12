c750e7181acaa69890b38b5b29a7ecb862e19e89
marcus <1175677541@qq.com>
2026-07-12 17:40:59 +0800

    multi add use f32

--- src/hgemv/hgemv.cuh
+++ src/hgemv/hgemv.cuh
@@ -4,16 +4,16 @@ __global__ void hgemv_k32_f16_kernel(half *A, half *x, half *y, int M, int K) {
     int m = blockIdx.y * blockDim.y + threadIdx.y;
     int laneId = threadIdx.x % WARP_SIZE;
     if (m < M) {
-        half sum = 0.0f;
+        float sum = 0.0f;
         int num_k_tiles = (K + WARP_SIZE - 1) / WARP_SIZE;
 #pragma unroll
         for (int tile = 0; tile < num_k_tiles; ++tile) {
             int k = tile * WARP_SIZE + laneId;
-            sum += A[m * K + k] * x[k];
+            sum += __half2float(A[m * K + k]) * __half2float(x[k]);
         }
-        sum = warp_reduce_sum_f16<WARP_SIZE>(sum);
+        sum = warp_reduce_sum_f32<WARP_SIZE>(sum);
         if (laneId == 0) {
-            y[m] = sum;
+            y[m] = __float2half(sum);
         }
     }
 }
@@ -23,21 +23,21 @@ __global__ void hgemv_k128_f16x4_kernel(half *A, half *x, half *y, int M,
     int m = blockIdx.y * blockDim.y + threadIdx.y;
     int laneId = threadIdx.x % WARP_SIZE;
     if (m < M) {
-        half sum = 0.0f;
+        float sum = 0.0f;
         int num_k_tiles = (((K + WARP_SIZE - 1) / WARP_SIZE) + 4 - 1) / 4;
 #pragma unroll
         for (int tile = 0; tile < num_k_tiles; ++tile) {
             int k = (tile * WARP_SIZE + laneId) * 4;
-            half2 reg_x_0 = HALF2(x[k + 0]);
-            half2 reg_x_1 = HALF2(x[k + 2]);
-            half2 reg_A_0 = HALF2(A[m * K + k + 0]);
-            half2 reg_A_1 = HALF2(A[m * K + k + 2]);
+            float2 reg_x_0 = __half22float2(HALF2(x[k + 0]));
+            float2 reg_x_1 = __half22float2(HALF2(x[k + 2]));
+            float2 reg_A_0 = __half22float2(HALF2(A[m * K + k + 0]));
+            float2 reg_A_1 = __half22float2(HALF2(A[m * K + k + 2]));
             sum += (reg_x_0.x * reg_A_0.x + reg_x_0.y * reg_A_0.y +
                     reg_x_1.x * reg_A_1.x + reg_x_1.y * reg_A_1.y);
         }
-        sum = warp_reduce_sum_f16<WARP_SIZE>(sum);
+        sum = warp_reduce_sum_f32<WARP_SIZE>(sum);
         if (laneId == 0) {
-            y[m] = sum;
+            y[m] = __float2half(sum);
         }
     }
 }
@@ -47,13 +47,15 @@ __global__ void hgemv_k16_f16_kernel(half *A, half *x, half *y, int M, int K) {
     constexpr int THREADS_PER_ROW =
         (WARP_SIZE + ROW_PER_WARP - 1) / ROW_PER_WARP;
     int laneId = threadIdx.x % WARP_SIZE;
+    int k = laneId % THREADS_PER_ROW;
     int m = (blockIdx.y * blockDim.y + threadIdx.y) * ROW_PER_WARP +
             laneId / THREADS_PER_ROW;
+    float sum = 0.0f;
     if (m < M) {
-        half sum = A[m * K + k] * x[k];
-        sum = warp_reduce_sum_f16<THREADS_PER_ROW>(sum);
-        if (k == 0) {
-            y[m] = sum;
-        }
+        sum = __half2float(A[m * K + k]) * __half2float(x[k]);
+    }
+    sum = warp_reduce_sum_f32<THREADS_PER_ROW>(sum);
+    if (k == 0 && m < M) {
+        y[m] = __float2half(sum);
     }
 }
--- src/hgemv/reduce.cuh
+++ src/hgemv/reduce.cuh
@@ -2,10 +2,10 @@
 #include <cuda_fp16.h>
 
 template <const int kWarpSize = WARP_SIZE>
-__device__ __forceinline__ half warp_reduce_sum_f16(half val) {
+__device__ __forceinline__ float warp_reduce_sum_f32(float val) {
 #pragma unroll
-    for (int stride = kWarpSize >> 1; stride >= 1; stride >>= 1) {
-        val += __shfl_xor_sync(0xffffffff, val, stride);
+    for (int mask = kWarpSize >> 1; mask >= 1; mask >>= 1) {
+        val += __shfl_xor_sync(0xffffffff, val, mask);
     }
     return val;
 }
